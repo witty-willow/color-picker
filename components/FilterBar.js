701e64da6a861ceabfb4d136b4e88182922935e8
Ethan Harry <Ethan@Ethans-MacBook-Pro.local>
2016-12-08 16:05:48 -0800

    Progress on color family filtering

--- components/FilterBar.js
+++ components/FilterBar.js
@@ -5,22 +5,21 @@ import React from 'react';
 var FilterBar = (props) => {
 
   var onFilterClick = (e) => {
-    //change to reflect function and state labels in main App
-    props.handleStateChange(props.selectedFilter)
+    props.handleStateChange(e)
   }
 
   return (
-    <Nav activeKey='1.4' bsStyle='pills' onSelect={onFilterClick}>
-      <NavDropdown eventKey='1' title='Dropdown' id='nav-dropdown'>
-        <MenuItem eventKey='1.1' title='menuItem'>Today</MenuItem>
-        <MenuItem eventKey='1.2' title='menuItem'>This Week</MenuItem>
-        <MenuItem eventKey='1.3' title='menuItem'>This Month</MenuItem>
-        <MenuItem eventKey='1.4' title='menuItem'>All-Time</MenuItem>
+    <Nav activeKey='allTime' bsStyle='pills' onSelect={onFilterClick}>
+      <NavDropdown eventKey='mostClicked' title='Dropdown' id='nav-dropdown'>
+        <MenuItem eventKey='today' title='menuItem'>Today</MenuItem>
+        <MenuItem eventKey='thisWeek' title='menuItem'>This Week</MenuItem>
+        <MenuItem eventKey='thisMonth' title='menuItem'>This Month</MenuItem>
+        <MenuItem eventKey='allTime' title='menuItem'>All-Time</MenuItem>
       </NavDropdown>
-      <NavItem title='Item' eventKey='2'>Blue</NavItem>
-      <NavItem title='Item' eventKey='3'>Red</NavItem>
-      <NavItem title='Item' eventKey='4'>Yellow</NavItem>
-      <NavItem title='Item' eventKey='5'>Green</NavItem>
+      <NavItem title='blue' eventKey='2'>Blue</NavItem>
+      <NavItem title='red' eventKey='3'>Red</NavItem>
+      <NavItem title='yellow' eventKey='4'>Yellow</NavItem>
+      <NavItem title='green' eventKey='5'>Green</NavItem>
     </Nav>
   )
 }
--- components/index.js
+++ components/index.js
@@ -2,14 +2,59 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import $ from 'jquery';
 import ColorFamilyView from './ColorFamilyView.js';
+import FilterBar from './FilterBar.js';
+import {Grid} from 'react-bootstrap';
+
+//make object {blue: {min=x, max=y}}
+
+
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       currentFilter: 'mostClicked',
+      filterRangeMin: 0,
+      filterRangeMax: 0,
       colorFamilies: []
     };
+
+    this.handleStateChange = this.handleStateChange.bind(this);
+  }
+
+
+
+  handleStateChange (color) {
+
+    var filteredColorFamilies = [];
+
+    this.setState({
+      currentFilter: color
+      // Need to determine how to access codes
+      // filterRangeMin: codes.color.min,
+      // filterRangeMax: codes.color.max, 
+    });
+
+    console.log('OUTER CONTEXT', this)
+  
+
+    this.state.colorFamilies.forEach(function (obj) {
+      var include = false;
+      for (var key in obj) {
+        if (key.slice(0,5) === 'color') {
+          var colorInt = parseInt(obj[key], 16);
+          if (colorInt > this.state.filterRangeMin && colorInt < this.state.filterRangeMax) {
+            include = true;
+          }
+        }
+      }
+      if (include === true) {
+        filteredColorFamilies.push(obj)
+      }
+    }.bind(this))
+    this.setState({
+      colorFamilies: filteredColorFamilies
+    });
   }
 
   componentWillMount() {
@@ -25,7 +70,10 @@ class App extends React.Component {
 
   render() {
     return (
-      <ColorFamilyView colorFamilies={this.state.colorFamilies}/>
+      <Grid>
+        <FilterBar handleStateChange={this.handleStateChange} currentFilter={this.state.currentFilter} />
+        <ColorFamilyView colorFamilies={this.state.colorFamilies}/>
+      </Grid>
     );
   }
 }
