ee74446b9a64b179d34bb1c0e1ce7056fa148ae3
Joosang Lee <armlessarcher@gmail.com>
2016-12-14 11:24:01 -0800

    Remove init comment from app, update colorcalc with some more functions

--- colorCalc.js
+++ colorCalc.js
@@ -3,7 +3,11 @@
 
 // console.log(color);
 // console.log(color.hex2rgb(193, 67, 28));
-
+var withinCircle = function(num) {
+  num = num > 360 ? num - 360 : num;
+  num = num < 0 ? num + 360 : num;
+  return num;
+};
 
 var RGBtoHSL = function(rgb) {
   /*rgb taken as array
@@ -31,7 +35,7 @@ var RGBtoHSL = function(rgb) {
   }
   H *= 60;
   return [H, S, L];
-}
+};
 
 // console.log(RGBtoHSL([24, 98, 118]));
 
@@ -60,51 +64,50 @@ var HSLtoRGB = function(hsl) {
     b = hue2rgb(p, q, h - 1 / 3);
   }
   return [r * 255, g * 255, b * 255];
-}
-
+};
 // console.log(HSLtoRGB(193, 67, 28));
 
 var componentToHex = function(c) {
   var hex = c.toString(16);
   return hex.length == 1 ? "0" + hex : hex;
-}
+};
 
 var RGBtoHex = function(r, g, b) {
   return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
-}
+};
 
 var HEXtoRGB = function(hex) {
-    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-    return result ? {
-      r: parseInt(result[1], 16),
-      g: parseInt(result[2], 16),
-      b: parseInt(result[3], 16)
-    } : null;
-  }
-  //////////////
-var complimentary = function(hsl) {
-  var h = hsl[0];
-  if (hsl[0] + 180 > 360) {
-    h -= 180;
-  } else {
-    h += 180;
-  }
-  return [h, hsl[1], hsl[2]];
+  var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+  return result ? {
+    r: parseInt(result[1], 16),
+    g: parseInt(result[2], 16),
+    b: parseInt(result[3], 16)
+  } : null;
+};
 
-}
+var complimentary = function(hsl) {
+  var results = [];
+  results.push(hsl);
+  var h = hsl[0];
+
+  h = withinCircle(h + 180);
+
+  results.push([h, hsl[1], hsl[2]]);
+  return results;
+};
+
 var triad = function(hsl) {
   var results = [];
   //takes [h,s,l]
   var h = hsl[0] - 120;
   var s = hsl[1];
   var l = hsl[2];
-  h = h < 0 ? h + 360 : h;
+  h = withinCircle(h)
   results.push([
     [h, s, l]
   ]);
   results.push(hsl);
-  h += 120
-  h = h > 360 ? h - 360 : h;
+  h = withinCircle(h - 120);
   results.push([h + 120, s, l]);
 
   return results;
@@ -115,37 +118,37 @@ var analagous = function(hsl) {
   var h = hsl[0];
   var s = hsl[1];
   var l = hsl[2];
-  h -= 30;
-  h = h < 0 ? h + 360 : h;
-  results.push(h, s, l);
-  h += 30;
-  results.push(h, s, l);
-  h += 30;
-  h = h > 360 ? h - 360 : h;
+
+  h = withinCircle(h - 30);
   results.push(h, s, l);
 
+  results.push(hsl);
+
+  h = withinCircle(h + 60);
+  results.push(h, s, l);
   return results;
-}
+};
 var splitComplimentary = function(hsl) {
   var results = [];
   var h = hsl[0];
   var s = hsl[1];
   var l = hsl[2];
-  var h1 = h - 150 > 0 ? h - 150 : h + 210;
-  var h2 = h + 150 > 360 ? h - 210 : h + 150;
+  var h1 = withinCircle(h - 150);
+  var h2 = withinCircle(h + 150);
+
   results.push([h1, s, l]);
   results.push([h, s, l]);
   results.push([h2, s, l]);
   return results;
-}
+};
 var squareTetrad = function(hsl) {
   var results = [];
   var h = hsl[0];
   var s = hsl[1];
   var l = hsl[2];
+
   for (var i = 0; i < 4; i++) {
-    h += 90;
-    h = h > 360 ? h - 360 : h;
+    h = withinCircle(h + 90);
 
     results.push([h, s, l])
   }
@@ -156,30 +159,54 @@ var monochromatic = function(hsl) {
   var h = hsl[0];
   var s = hsl[1];
   var l = hsl[2];
+
   for (var i = 0; i <= 100; i += 25) {
     results.push([h, s, i]);
   }
   return results;
 };
-var cool = function(hsl){
+var rectangularTetrad = function(hsl) {
+  //-30, + 150, + 180;
+  var results = [];
+  var h = hsl[0];
+  var s = hsl[1];
+  var l = hsl[2];
+
+  var temp = withinCircle(h - 30);
+  results.push([temp, s, l]);
+
+  results.push(hsl);
+
+  temp = withinCircle(h + 150);
+  results.push([temp, s, l]);
+
+  temp = withinCircle(h + 180);
+  results.push([temp, s, l])
+
+  return results;
+};
+
+//May or may not do this   //probably not going to do this.
+var cool = function(hsl) {
   var results = [];
   var h = hsl[0];
   var s = hsl[1];
   var l = hsl[2];
 };
-var warm = function(hsl){
+var warm = function(hsl) {
   var results = [];
   var h = hsl[0];
   var s = hsl[1];
   var l = hsl[2];
 };
+console.log(rectangularTetrad([350, 0, 0]));
 // console.log(analagous([350, 0, 0]));
 // console.log(complimentary([193, 67, 28]));
 // console.log(triad([193, 67, 28]));
 // console.log(splitComplimentary([100,0,0]));
 // console.log(squareTetrad([193, 67, 28]));
 // console.log(monochromatic([193, 67, 28]));
-
+module.exports.rectangularTetrad = rectangularTetrad;
 module.exports.complimentary = complimentary;
 module.exports.triad = triad;
 module.exports.analagous = analagous;
