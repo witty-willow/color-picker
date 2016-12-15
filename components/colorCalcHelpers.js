b596b5bdb3105432438d68dee5dcb82b96da4f82
witygass <witygass@indiana.edu>
2016-12-14 19:58:19 -0800

    closing shop

--- components/colorCalcHelpers.js
+++ components/colorCalcHelpers.js
@@ -208,11 +208,49 @@ var cool = function(hsl) {
   var s = hsl[1];
   var l = hsl[2];
 };
+
 var warm = function(hsl) {
   var results = [];
   var h = hsl[0];
   var s = hsl[1];
   var l = hsl[2];
 };
+
+// Helper for selecting font, currently unused!!!!!!
+// var c = document.getElementById('container');
+//
+// var colourIsLight = function (r, g, b) {
+//
+//   // Counting the perceptive luminance
+//   // human eye favors green color...
+//   var a = 1 - (0.299 * r + 0.587 * g + 0.114 * b) / 255;
+//   console.log(a);
+//   return (a < 0.5);
+// }
+//
+// var randomRgb = function () {
+//   var r = /* 189; //*/ Math.floor(Math.random() * 256);
+//   var g = /*60; //*/ Math.floor(Math.random() * 256);
+//   var b = /*151; //*/ Math.floor(Math.random() * 256);
+//   return [r, g, b];
+// };
+//
+// var colourFromRgb = function (r, g, b) {
+//   return 'rgb(' + r + ',' + g + ',' + b + ')';
+// };
+//
+// for (var i = 0; i < 1000; i += 1) {
+//   var el = document.createElement('div');
+//   el.setAttribute('class', 'box');
+//   el.textContent = "Hello";
+//
+//   var bgRgb = randomRgb();
+//   var bgColour = colourFromRgb(bgRgb[0], bgRgb[1], bgRgb[2]);
+//   var textColour = colourIsLight(bgRgb[0], bgRgb[1], bgRgb[2]) ? 'black' : 'white';
+//
+//   el.setAttribute('style', 'background-color: ' + bgColour + '; color: ' + textColour);
+//
+//   c.appendChild(el);
+// }
 
 export {HEXtoRGB, RGBtoHSL};
