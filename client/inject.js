570e776ce933e433e850b4294030a731a4676cb5
Emmeline <Emmeline.Lan@gmail.com>
2016-12-14 17:17:10 -0800

    progress on color change extension

---
+++ client/inject.js
@@ -0,0 +1,71 @@
+console.log('chrome extension is running')
+// This helps avoid conflicts in case we inject 
+// this script on the same page multiple times
+// without reloading.
+var hexToRGB = function(hex) {
+  var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+  return result ? {
+    r: parseInt(result[1], 16),
+    g: parseInt(result[2], 16),
+    b: parseInt(result[3], 16)
+  } : null;
+}
+
+var colorFamily2 = {
+    color1: hexToRGB('#2DE1FC'),
+    color2: hexToRGB('#2AFC98'),
+    color3: hexToRGB('#09E85E'),
+    color4: hexToRGB('#16C172'),
+    color5: hexToRGB('#214F4B')
+  }
+
+var colorFamily = {
+    color1: '#2DE1FC',
+    color2: '#2AFC98',
+    color3: '#09E85E',
+    color4: '#16C172',
+    color5: '#214F4B'
+  }
+
+
+
+// var injected = injected || (function(){
+
+//   // An object that will contain the "methods"
+//   // we can use from our event script.
+//   var methods = {};
+
+  // This method will eventually return
+  // background colors from the current page.
+var getBgColors = function(){
+    var colors = {};
+    var nodes = document.querySelectorAll('*');
+    var node, nodeArea, bgColor, i;
+
+    var divs = nodes;
+
+    for (var i = 0; i < divs.length; i++) {
+      var currentKey = 'color' + (i % 5 + 1);
+      divs[i].style['background-color'] = colorFamily[currentKey];
+    }
+
+    return 'Hello' + divs.length;
+  };
+
+chrome.runtime.onMessage.addListener(getBgColors);
+  // This tells the script to listen for
+  // messages from our extension.
+//   chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+//     var data = {};
+//     // If the method the extension has requested
+//     // exists, call it and assign its response
+//     // to data.
+//     if (methods.hasOwnProperty(request.method))
+//       data = methods[request.method]();
+//     // Send the response back to our extension.
+//     sendResponse({ data: data });
+//     return true;
+//   });
+
+//   return true;
+// })();
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,13 @@
 var path = require('path');
 
 module.exports = {
-  entry: './components/index.js',
+  entry: {
+    main: './components/index.js',
+    ext: './components/extensionColorFamily.js'
+  },
   output: {
     path: path.resolve(__dirname, 'client/dist'),
-    filename: 'bundle.js'
+    filename: '[name].bundle.js'
   },
   module: {
     loaders: [
