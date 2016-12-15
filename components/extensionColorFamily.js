f03b3b4bee6604e0000cdfb970dc171a792a77a3
Emmeline <Emmeline.Lan@gmail.com>
2016-12-14 18:24:43 -0800

    cleaned up chrome extension to run with rebase

--- components/extensionColorFamily.js
+++ components/extensionColorFamily.js
@@ -5,21 +5,13 @@ import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 import ColorFamily from './ColorFamily';
   
 
-var colorFamily = {
-<<<<<<< HEAD
-    color1: '#2DE1FC',
-    color2: '#2AFC98',
-    color3: '#09E85E',
-    color4: '#16C172',
-    color5: '#214F4B'
-=======
-    color1: { hex: '#2DE1FC' },
-    color2: { hex: '#2AFC98' },
-    color3: { hex: '#09E85E' },
-    color4: { hex: '#16C172' },
-    color5: { hex: '#214F4B' }
->>>>>>> 88efd6f0389fad5a578dfe24e94a334a70f8a3ae
-  }
+var palette = {
+  color1: {name: 'Cyan', hex: '#2DE1FC'},
+  color2: {name: 'Spring Green', hex: '#2AFC98'},
+  color3: {name: 'Malachite', hex: '#09E85E'},
+  color4: {name: 'Mountain Meadow', hex: '#16C172'},
+  color5: {name: 'Blue Dianne', hex: '#214F4B'},
+}
 
 var hexToRGB = function(hex) {
   var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
@@ -31,19 +23,11 @@ var hexToRGB = function(hex) {
 }
 
 var colorFamily2 = {
-<<<<<<< HEAD
-    color1: hexToRGB('#2DE1FC'),
-    color2: hexToRGB('#2AFC98'),
-    color3: hexToRGB('#09E85E'),
-    color4: hexToRGB('#16C172'),
-    color5: hexToRGB('#214F4B')
-=======
     color1: '#2DE1FC',
     color2: '#2AFC98',
     color3: '#09E85E',
     color4: '#16C172',
     color5: '#214F4B'
->>>>>>> 88efd6f0389fad5a578dfe24e94a334a70f8a3ae
   }
 
 console.log(colorFamily2);
@@ -56,11 +40,7 @@ class ExtensionColorFamily extends React.Component {
     this.state = {
       greeting: 'hello'
     }
-<<<<<<< HEAD
-=======
-
     this.getBrowserColors = this.getBrowserColors.bind(this)
->>>>>>> 88efd6f0389fad5a578dfe24e94a334a70f8a3ae
   }
 
   componentWillMount(){
@@ -71,11 +51,7 @@ class ExtensionColorFamily extends React.Component {
 
   getBrowserColors() {
     console.log('clicked')
-<<<<<<< HEAD
-    var msg = 'getBgColors';
-=======
     var msg = this.state.colorFamily;
->>>>>>> 88efd6f0389fad5a578dfe24e94a334a70f8a3ae
     var params = {
       active: true,
       currentWindow: true    
@@ -86,35 +62,21 @@ class ExtensionColorFamily extends React.Component {
     // Now we've got the tabs
     function gotTabs(tabs) {
       // The first tab is the one you are on
-<<<<<<< HEAD
-      chrome.tabs.sendMessage(tabs[0].id, msg);//, messageBack);
-=======
       chrome.tabs.sendMessage(tabs[0].id, {currentColors: msg});//, messageBack);
->>>>>>> 88efd6f0389fad5a578dfe24e94a334a70f8a3ae
     }
   }
 
   render() {
     return (
       <div>
-<<<<<<< HEAD
-        {this.state.greeting}
-        <button onClick={this.getBrowserColors}>click me</button>
-        <ColorFamily colorFamily={this.state.colorFamily}/>
-=======
         <ColorFamily colorFamily={this.state.colorFamily}/>
         <button onClick={this.getBrowserColors}>Apply Colors</button>
->>>>>>> 88efd6f0389fad5a578dfe24e94a334a70f8a3ae
       </div>
     )
   }
 }
 
 ReactDOM.render(
-<<<<<<< HEAD
-  <ExtensionColorFamily colorFamily={colorFamily}/>,
-=======
-  <ExtensionColorFamily colorFamily={colorFamily2}/>,
->>>>>>> 88efd6f0389fad5a578dfe24e94a334a70f8a3ae
+  <ExtensionColorFamily colorFamily={palette}/>,
   document.getElementById('extensionBody')
 );
